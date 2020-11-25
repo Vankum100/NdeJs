320c2472599a9c1d3092bb099be31858da87d4a3
Vankum100 <kumkarana04@gmail.com>
2020-11-25 15:06:36 +0300

    static files and routing completed

---
+++ library-app/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const chalk =  require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+
+const app = express();
+
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+const nav = [
+    { link: '/books', title: 'Book' },
+    { link: '/authors', title: 'Author' }
+  ];
+  
+  const bookRouter = require('./src/routes/bookRoutes')(nav);
+  
+  app.use('/books', bookRouter);
+  app.get('/', (req, res) => {
+    res.render(
+      'index',
+      {
+        nav: [{ link: '/books', title: 'Books' },
+        { link: '/authors', title: 'Authors' }],
+        title: 'Library'
+      }
+    );
+  });
+  
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => debug(`Server Running at Port ${chalk.green(PORT)}`));
