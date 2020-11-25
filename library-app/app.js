0aac53b353e571dbde7507c59038f2581d22f096
Vankum100 <kumkarana04@gmail.com>
2020-11-25 20:51:12 +0300

    added database connection and admin route

--- library-app/app.js
+++ library-app/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const chalk =  require('chalk');
+const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
 
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
@@ -14,27 +15,29 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+
 const nav = [
-    { link: '/books', title: 'Book' },
-    { link: '/authors', title: 'Author' }
-  ];
-  
-  const bookRouter = require('./src/routes/bookRoutes')(nav);
-  
-  app.use('/books', bookRouter);
-  app.get('/', (req, res) => {
-    res.render(
-      'index',
-      {
-        nav: [{ link: '/books', title: 'Books' },
-        { link: '/authors', title: 'Authors' }],
-        title: 'Library'
-      }
-    );
-  });
-  
+  { link: '/books', title: 'Book' },
+  { link: '/authors', title: 'Author' }
+];
 
+const bookRouter = require('./src/routes/bookRoutes')(nav);
+const adminRouter = require('./src/routes/adminRoutes')(nav);
 
-const PORT = process.env.PORT || 3000;
+app.use('/books', bookRouter);
+app.use('/admin', adminRouter);
 
-app.listen(PORT, () => debug(`Server Running at Port ${chalk.green(PORT)}`));
+app.get('/', (req, res) => {
+  res.render(
+    'index',
+    {
+      nav: [{ link: '/books', title: 'Books' },
+      { link: '/authors', title: 'Authors' }],
+      title: 'Library'
+    }
+  );
+});
+
+app.listen(port, () => {
+  debug(`listening on port ${chalk.green(port)}`);
+});
