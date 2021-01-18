988ae0fed77b19d77be340c95186dcf67b8fc348
Vankum100 <kumkarana04@gmail.com>
2021-01-18 22:19:23 +0300

    adding nest app
    added local nest app

---
+++ nest-api/src/products/products.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsService } from './products.service';
+import { ProductsController } from './products.controller';
+import { Product, ProductSchema } from './schemas/products.schema';
+
+@Module({
+  providers: [ProductsService],
+  controllers: [ProductsController],
+  imports: [
+    MongooseModule.forFeature([
+      {name: Product.name, schema: ProductSchema}
+    ])
+  ]
+})
+export class ProductsModule {
+}
+
+//  separate model for products
